9d568705fa82edf86035ab4ff4ffd2f1794fa235
coronasauras <harry.chenjun.jing@gmail.com>
2025-09-19 16:50:25 -0600

    Added project code

---
+++ code/app/src/main/java/com/example/listycitylab3/EditCityFragment.java
@@ -0,0 +1,53 @@
+package com.example.listycitylab3;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.appcompat.app.AlertDialog;
+
+public class EditCityFragment extends DialogFragment {
+
+    private static final String CITY_KEY = "city";
+
+    public static EditCityFragment newInstance(City city) {
+        EditCityFragment fragment = new EditCityFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(CITY_KEY, city);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final City city = (City) getArguments().getSerializable(CITY_KEY);
+
+        LinearLayout layout = new LinearLayout(getActivity());
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        EditText cityNameEditText = new EditText(getActivity());
+        EditText provinceEditText = new EditText(getActivity());
+
+        cityNameEditText.setText(city.getName());
+        provinceEditText.setText(city.getProvince());
+
+        layout.addView(cityNameEditText);
+        layout.addView(provinceEditText);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Add/Edit City");
+        builder.setView(layout);
+
+        builder.setPositiveButton("OK", (dialog, id) -> {
+            city.setName(cityNameEditText.getText().toString());
+            city.setProvince(provinceEditText.getText().toString());
+            ((MainActivity) getActivity()).updateCity(city);
+        });
+
+        builder.setNegativeButton("Cancel", (dialog, id) -> {});
+
+        return builder.create();
+    }
+}
--- code/app/src/main/java/com/example/listycitylab3/MainActivity.java
+++ code/app/src/main/java/com/example/listycitylab3/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.listycitylab3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,26 +14,63 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> dataList;
+    private ArrayList<City> dataList;
     private ListView cityList;
-    private ArrayAdapter<String> cityAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] cities = {
-                "Edmonton", "Vancouver", "Moscow",
-                "Sydney", "Berlin", "Vienna",
-                "Tokyo", "Beijing", "Osaka", "New Delhi"
+        City[] cities = {
+                new City("Edmonton", "Alberta"),
+                new City("Vancouver", "British Columbia"),
+                new City("Moscow", "Moscow Oblast"),
+                new City("Sydney", "New South Wales"),
+                new City("Berlin", "Berlin"),
+                new City("Vienna", "Vienna"),
+                new City("Tokyo", "Tokyo"),
+                new City("Beijing", "Beijing"),
+                new City("Osaka", "Osaka"),
+                new City("New Delhi", "Delhi")
         };
 
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(cities));
-        
+
         cityList = findViewById(R.id.city_list);
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+
+        ArrayAdapter<City> cityAdapter = new ArrayAdapter<City>(this, android.R.layout.simple_list_item_1, dataList) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                City city = getItem(position);
+                if (convertView == null) {
+                    convertView = getLayoutInflater().inflate(android.R.layout.simple_list_item_1, parent, false);
+                }
+
+                TextView textView = convertView.findViewById(android.R.id.text1);
+                textView.setText(city.getName() + ", " + city.getProvince());
+
+                return convertView;
+            }
+        };
+
         cityList.setAdapter(cityAdapter);
+
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            City selectedCity = dataList.get(position);
+            EditCityFragment fragment = EditCityFragment.newInstance(selectedCity);
+            fragment.show(getSupportFragmentManager(), "editCity");
+        });
+    }
+
+    public void updateCity(City updatedCity) {
+        for (int i = 0; i < dataList.size(); i++) {
+            if (dataList.get(i).getName().equals(updatedCity.getName())) {
+                dataList.set(i, updatedCity);
+                ((ArrayAdapter) cityList.getAdapter()).notifyDataSetChanged();
+                return;
+            }
+        }
     }
 }
